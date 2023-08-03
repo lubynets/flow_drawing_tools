d4962b5a0ca9b42c037666ec407de4bacbb9e142
Oleksii Lubynets <lubinets95@gmail.com>
2023-08-03 14:55:09 +0200

    untrack macro (which is ignored)

--- macro/v1_complex.h
+++
@@ -1,7 +0,0 @@
-template <typename T>
-std::string to_string_with_precision(const T a_value, const int n = 6) {
-  std::ostringstream out;
-  out.precision(n);
-  out << std::fixed << a_value;
-  return out.str();
-}
